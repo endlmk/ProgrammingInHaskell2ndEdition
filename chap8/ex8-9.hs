2a55d6a15c01d91a05d23e7e3291de4d76e40508
endlmk <1269467+endlmk@users.noreply.github.com>
2021-06-29 22:52:41 +0900

    Done chap8.

---
+++ chap8/ex8-9.hs
@@ -0,0 +1,19 @@
+data Expr = Val Int | Add Expr Expr | Mult Expr Expr
+
+type Cont = [Op]
+data Op = EVAL_ADD Expr | EVAL_MULT Expr | ADD Int | MULT Int 
+
+eval :: Expr -> Cont -> Int 
+eval (Val n) c = exec c n
+eval (Add x y) c = eval x (EVAL_ADD y : c)
+eval (Mult x y) c = eval x (EVAL_MULT y : c)
+
+exec :: Cont -> Int -> Int 
+exec [] n = n
+exec (EVAL_ADD y : c) n =  eval y (ADD n : c)
+exec (EVAL_MULT y : c) n =  eval y (MULT n : c)
+exec (ADD n : c) m  = exec c (n + m)
+exec (MULT n : c) m  = exec c (n * m)
+
+value :: Expr -> Int 
+value e = eval e []
